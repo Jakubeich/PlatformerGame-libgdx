423cabde138d692a144c038588b7e5906c845549
Jakub Mitrega <jakubmitrega1@gmail.com>
2021-05-20 15:25:39 +0200

    Tile manager

---
+++ desktop/src/com/jakubeich/platformergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jakubeich.platformergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jakubeich.platformergame.PlatformerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerGame(), config);
+	}
+}
